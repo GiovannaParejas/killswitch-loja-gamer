10f11992e36ae51a1c300ba2ad9185dda7b89bfe
Giovanna <gparejas23@outlook.com>
2026-05-19 17:08:35 -0300

    feat: Adicionando explorar por categoria

--- src/js/script.js
+++ src/js/script.js
@@ -46,3 +46,15 @@ const intervalo = setInterval(() => {
 }, 60);
 
 // Fim: Loading
+
+// Início: Menu
+
+const hamburguer = document.getElementById('hamburguer');
+const menu = document.getElementById('link-menu');
+
+hamburguer.addEventListener('click', () => {
+    menu.classList.toggle('aberto');
+    hamburguer.classList.toggle('aberto');
+});
+
+// Fim: Menu
